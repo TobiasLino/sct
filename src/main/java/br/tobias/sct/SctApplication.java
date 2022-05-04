2675f5d07857fdfe1767e847c50404b5ee6ffdfd
TobiasLino <linop019@gmail.com>
2022-05-04 20:49:25 -0300

    first commit

---
+++ src/main/java/br/tobias/sct/SctApplication.java
@@ -0,0 +1,13 @@
+package br.tobias.sct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SctApplication.class, args);
+	}
+
+}
